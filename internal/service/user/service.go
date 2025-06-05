39f13810fbef7225c65dad101e2e805618183753
yulia.gopachenko@gmail.com <yulia.gopachenko@gmail.com>
2025-06-05 18:28:40 +0100

    [HMJ-25] Add us client  methods

--- internal/service/user/service.go
+++ internal/service/user/service.go
@@ -3,6 +3,7 @@ package user
 import (
 	"context"
 	"github.com/handmade-jewelry/auth-service/logger"
+	"google.golang.org/protobuf/types/known/emptypb"
 
 	grpcRetry "github.com/grpc-ecosystem/go-grpc-middleware/retry"
 	"google.golang.org/grpc"
@@ -36,30 +37,87 @@ func NewService(opts *config.GRPCOptions) (*Service, error) {
 }
 
 func (s *Service) UserRoles(ctx context.Context, userID int64) ([]string, error) {
+	req := &userService.GetUserRolesRequest{
+		UserId: userID,
+	}
+
+	res, err := s.client.GetUserRoles(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+
+	roles := make([]string, 0, len(res.GetRoles()))
+	for _, role := range res.GetRoles() {
+		roles = append(roles, role.GetName())
+	}
+
+	return roles, nil
+
 	//todo stub
-	return []string{"CUSTOMER"}, nil
+	//return []string{"CUSTOMER"}, nil
 }
 
-func (s *Service) RoleMap(ctx context.Context) (map[string]string, error) {
+func (s *Service) RoleMap(ctx context.Context) (map[string]struct{}, error) {
+	res, err := s.client.ListRoles(ctx, &emptypb.Empty{})
+	if err != nil {
+		return nil, err
+	}
+
+	roles := make(map[string]struct{}, len(res.GetRoles()))
+	for _, role := range res.GetRoles() {
+		name := role.GetName()
+		roles[name] = struct{}{}
+	}
+
+	return roles, nil
+
 	//todo stub
-	return map[string]string{
-		"CUSTOMER": "CUSTOMER",
-		"ADMIN":    "ADMIN",
-		"SELLER":   "SELLER",
-	}, nil
+	//return map[string]struct{}{
+	//	"CUSTOMER": {},
+	//	"ADMIN":    {},
+	//	"SELLER":   {},
+	//}, nil
 }
 
 func (s *Service) RoleList(ctx context.Context) ([]string, error) {
+	res, err := s.client.ListRoles(ctx, &emptypb.Empty{})
+	if err != nil {
+		return nil, err
+	}
+
+	roles := make([]string, 0, len(res.GetRoles()))
+	for _, role := range res.GetRoles() {
+		roles = append(roles, role.GetName())
+	}
+
+	return roles, nil
+
 	//todo stub
-	//todo get from cashe
-	return []string{"CUSTOMER", "ADMIN", "SELLER"}, nil
-	//todo set from cashe
+	//return []string{"CUSTOMER", "ADMIN", "SELLER"}, nil
 }
 
 func (s *Service) Login(ctx context.Context, email, password string) (*UserWithRoles, error) {
-	//todo stub
+	res, err := s.client.Login(ctx, &userService.LoginRequest{
+		Email:    email,
+		Password: password,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	roles := make([]string, 0, len(res.GetRoles()))
+	for _, role := range res.GetRoles() {
+		roles = append(roles, role.GetName())
+	}
+
 	return &UserWithRoles{
-		UserID: 1,
-		Roles:  []string{"CUSTOMER"},
+		UserID: res.GetUserId(),
+		Roles:  roles,
 	}, nil
+
+	//todo stub
+	//return &UserWithRoles{
+	//	UserID: 1,
+	//	Roles:  []string{"CUSTOMER"},
+	//}, nil
 }
